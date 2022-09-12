05be844aa8a823d94a019d5af3c3fc24d5d0e949
Fahad <fadiktk000@gmail.com>
2022-09-12 16:02:44 +0500

    updates

--- lib/utils/src/compass_ui.dart
+++ lib/utils/src/compass_ui.dart
@@ -4,51 +4,74 @@ import 'package:flutter/material.dart';
 import '../smooth_compass.dart';
 double preValue=0;
 double turns=0;
+///custom callback for building widgets
 typedef WidgetBuilder=Widget Function(BuildContext context,AsyncSnapshot<CompassModel>? compassData,Widget compassAsset);
 class SmoothCompass extends StatelessWidget {
   final WidgetBuilder compassBuilder;
   final Widget? compassAsset;
+  final Widget? loadingAnimation;
   final int? rotationSpeed;
   final double? height;
   final double? width;
 
-  const SmoothCompass({Key? key,required this.compassBuilder,this.compassAsset, this.rotationSpeed=200, this.height=200, this.width=200}) : super(key: key);
+  const SmoothCompass({Key? key,required this.compassBuilder,this.compassAsset, this.rotationSpeed=200, this.height=200, this.width=200, this.loadingAnimation}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<CompassModel>(
-      stream: Compass().compassUpdates(interval: const Duration(milliseconds: -1,),azimuthFix: 0.0),
-      builder: (context,AsyncSnapshot<CompassModel> snapshot){
-        if(compassAsset==null){
-          if(snapshot.connectionState==ConnectionState.waiting)
+    /// check if the compass support available
+    return FutureBuilder(
+      future: Compass().isCompassAvailable(),
+      builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+        if(snapshot.connectionState==ConnectionState.waiting)
           {
-            return const CircularProgressIndicator();
+            return loadingAnimation!=null ?loadingAnimation!: const Center(
+              child: CircularProgressIndicator(),
+            );
           }
-          if(snapshot.hasError)
+        if(!snapshot.data!)
           {
-            return Text(snapshot.error.toString());
+            return const Center(
+                child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                    child: Text("Compass support for this device is not available"))
+            );
           }
-          return compassBuilder(context,snapshot,_defaultWidget(snapshot));
-        }
-        else{
-          if(snapshot.connectionState==ConnectionState.waiting)
-          {
-            return const CircularProgressIndicator();
-          }
-          if(snapshot.hasError)
-          {
-            return Text(snapshot.error.toString());
-          }
-          return compassBuilder(context,snapshot,AnimatedRotation(
-            turns: snapshot.data!.turns*-1, duration: Duration(milliseconds: rotationSpeed!),
-            child: compassAsset!,
-          ),
-          );
-        }
-      },
+        /// start compass stream
+        return StreamBuilder<CompassModel>(
+          stream: Compass().compassUpdates(
+              interval: const Duration(milliseconds: -1,), azimuthFix: 0.0),
+          builder: (context, AsyncSnapshot<CompassModel> snapshot) {
+            if (compassAsset == null) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return loadingAnimation!=null? loadingAnimation!: const CircularProgressIndicator();
+              }
+              if (snapshot.hasError) {
+                return Text(snapshot.error.toString());
+              }
+              return compassBuilder(
+                  context, snapshot, _defaultWidget(snapshot));
+            }
+            else {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              }
+              if (snapshot.hasError) {
+                return Text(snapshot.error.toString());
+              }
+              return compassBuilder(context, snapshot, AnimatedRotation(
+                turns: snapshot.data!.turns * -1,
+                duration: Duration(milliseconds: rotationSpeed!),
+                child: compassAsset!,
+              ),
+              );
+            }
+          },
+        );
+      }
+
     );
   }
-
+///default widget if custom widget isn't provided
   Widget _defaultWidget(AsyncSnapshot<CompassModel> snapshot)
   {
     return AnimatedRotation(
@@ -68,6 +91,8 @@ class SmoothCompass extends StatelessWidget {
   }
 }
 
+
+///calculating compass turn from heading value
 getCompassValues(double heading)
 {
   double direction = heading;
@@ -88,7 +113,7 @@ getCompassValues(double heading)
   preValue = direction;
   return CompassModel(turns: turns, angle: heading);
 }
-
+/// model to store the sensor value
 class CompassModel{
   double turns;
   double angle;
